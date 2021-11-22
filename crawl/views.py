8e584a52d9d4073b0eaec37e7bfc25058bae5207
Aman Mishra <aman777444@gmail.com>
2021-11-22 18:07:33 +0100

    debugged views exception part

--- crawl/views.py
+++ crawl/views.py
@@ -187,21 +187,10 @@ def email_pdf(request):
             "name": "Aman Mishra",
             }
         ],
-        "lists": [],
-        "contacts": [],
-        "attachments": [],
-        "title": "Title",
-        "html": "Body",
-        "methods": {
-            "postmark": False,
-            "secureSend": False,
-            "encryptContent": False,
-            "secureReply": False
-        }
         })
         headers = {
-        'x-trustifi-key': '{{fff4ae6104486fc20de26cb0501f4310c663f9c0cbc8bf49}}',
-        'x-trustifi-secret': '{{0f7c288aad8a9542f1c355b60b05e0f0}}',
+        'x-trustifi-key': 'fff4ae6104486fc20de26cb0501f4310c663f9c0cbc8bf49',
+        'x-trustifi-secret': '0f7c288aad8a9542f1c355b60b05e0f0',
         'Content-Type': 'application/json'
         }
         conn.request("POST", url, payload, headers)
