94fb456306a4c291044f9a709c8862bc12fcdcef
Aman Mishra <aman777444@gmail.com>
2021-11-23 11:10:09 +0100

    put email function inside crawl function

--- crawl/urls.py
+++ crawl/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='home'),
-    path('initiate',views.crawl,name='crawl'),
-    path('email',views.email_pdf,name='emailpdf')
+    path('initiate',views.crawl,name='crawl')
+    # path('email',views.email_pdf,name='emailpdf')
 ]
 
