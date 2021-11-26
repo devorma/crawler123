e39555b4308e03b10de18b76df4f09f2cfef2786
Aman Mishra <aman777444@gmail.com>
2021-11-26 09:40:29 +0100

    Modified search script and relocated database scriptinside script

--- crawl/views.py
+++ crawl/views.py
@@ -29,7 +29,7 @@ import smtplib
  
 from .models import Publisher
 import urllib
-
+from urllib.request import urlopen
 
 
 #mailscript libraries
@@ -47,6 +47,7 @@ import email.utils
 #importing libraries for reading pdf and spacy
 import PyPDF2 as pdf
 import spacy
+from StringIO import StringIO
 
 from spacy.lang.it.stop_words import STOP_WORDS as stopwords #getting the italian stop words
 
@@ -91,38 +92,42 @@ def crawl(request):
                 ### NLP SPACY
                 String = "La Camera di Commercio territorialmente competente effettuerà controlli su un campione pari" #the search string from a pdf 
                 print('The search string is :\n',String)
-                pdf_reader= pdf.PdfFileReader(a+ ".pdf",'rb')
+                
+                file_read=urlopen(u).read()
+                print('The pdf with urlopen is:\n',file_read)
+                
+                file = open(fl + ".pdf", 'wb')
+                pdf_reader= pdf.PdfFileReader(file(u, "rb"))
                 print('The pdf is:\n',pdf_reader)
-                #input1 = pdf.PdfFileReader(file("http://example.com/a.pdf", "rb"))
-                if pdf_reader.getNumPages()>0: #pdf_reader.getIsEncrypted()=='False' and
-                    print('The condition is met for the pdf named:\n',a)
-                    NumPages = pdf_reader.getNumPages()
-                    for i in range(0,NumPages):
-                        PageObj = pdf_reader.getPage(i)
-                        print("This is page " + str(i))
-                        Text = PageObj.extractText() #Extracting the text from the pdf page and then using the nlp to find the matching pattersns in the later stage 
-                        print(f'The extracted text from the page {i} is :\n{Text}')
-                        ResSearch = re.search(String, Text)
-                        print('The ResSearch result is :\n',ResSearch)
-                        if ResSearch =='True':
-                            file_size=response.headers.get('content-length', 0)
-                            content_type=response.headers.get('Content-Type', 0)
-                            last_modified=response.headers.get('Last-Modified', 0)
-                            expiry_date=response.headers.get('Expires', 0)
-                            cache_control=response.headers.get('Cache', 0)
-                            server=response.headers.get('Server', 0)
+                
+                NumPages = pdf_reader.getNumPages()
+                print('The number of pages in the pdf are:\n',NumPages)
+                for i in range(0,NumPages):
+                    PageObj = pdf_reader.getPage(i)
+                    print("This is page " + str(i))
+                    Text = PageObj.extractText() #Extracting the text from the pdf page and then using the nlp to find the matching pattersns in the later stage 
+                    print(f'The extracted text from the page {i} is :\n{Text}')
+                    ResSearch = re.search(String, Text)
+                    print('The ResSearch result is :\n',ResSearch)
+                    # if ResSearch =='True':
+                    file_size=response.headers.get('content-length', 0)
+                    content_type=response.headers.get('Content-Type', 0)
+                    last_modified=response.headers.get('Last-Modified', 0)
+                    expiry_date=response.headers.get('Expires', 0)
+                    cache_control=response.headers.get('Cache', 0)
+                    server=response.headers.get('Server', 0)
 
-                            pub_instance = Publisher.objects.create(name=fl,links=u,file_size=file_size,content_type=content_type,last_modified_y=last_modified,expiry_date_y=expiry_date,cache_control_y=cache_control,server_y=server) #creating the entry in the database
-                            pub_instance.save() #saving the info for each url to database
+                    pub_instance = Publisher.objects.create(name=fl,links=u,file_size=file_size,content_type=content_type,last_modified_y=last_modified,expiry_date_y=expiry_date,cache_control_y=cache_control,server_y=server) #creating the entry in the database
+                    pub_instance.save() #saving the info for each url to database
 
-                            for row in Publisher.objects.all().reverse(): #removing all the duplicate items from the database
-                                if Publisher.objects.filter(name=row.name).count() > 1: #using name as a filter
-                                    print('Found Duplicate item:\n',row.name)
-                                    row.delete()
-                    pass #This will only execute the unencrypted files and valid files which contain the search string
-                else:
-                    break
-                ###
+                    for row in Publisher.objects.all().reverse(): #removing all the duplicate items from the database
+                        if Publisher.objects.filter(name=row.name).count() > 1: #using name as a filter
+                            print('Found Duplicate item:\n',row.name)
+                            row.delete()
+                
+                    #This will only execute the unencrypted files and valid files which contain the search string
+
+                ###The previous v1.0 version is codeed below
                 # file_size=response.headers.get('content-length', 0)
                 # content_type=response.headers.get('Content-Type', 0)
                 # last_modified=response.headers.get('Last-Modified', 0)
@@ -140,6 +145,7 @@ def crawl(request):
     except Exception as exc:
         pass
         return render(request, 'result.html', {'list':all_links}) #redirecting to the results template page
+        #if integrity exception error is passed or value error is thrown
 
 
 
@@ -154,10 +160,11 @@ def email_pdf(request):
 
     payload = "{\"recipients\":[{\"email\":\"aman777444@gmail.com\"}],\"title\":\"Crawler Result Dialy Mails\",\"html\":\"I hope you are well. These are the links which have been generated for your convinience.<br>"+''.join(extracted_links)+"<p>Regards,<br>Aman Mishra</p>\"}"
     headers = {
-    'x-trustifi-key': os.environ['TRUSTIFI_KEY'],
-    'x-trustifi-secret': os.environ['TRUSTIFI_SECRET'],
-    'Content-Type': 'application/json'
+    'x-trustifi-key': os.environ['TRUSTIFI_KEY'], #automatically retrieves the key from the environment from os library
+    'x-trustifi-secret': os.environ['TRUSTIFI_SECRET'], #this is also retrieved from os library
+    'Content-Type': 'application/json' #the type of file we are passing 
     }
     
     response = requests.request('POST', url, headers = headers, data = payload)
     return render(request, 'email_result.html')
+
